33b74cede100f3433f979339d222ff511538f422
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2024-02-27 21:52:17 +0530

    fix: mpv configure

--- packages/mpv-release.cmake
+++ packages/mpv-release.cmake
@@ -53,7 +53,6 @@ ExternalProject_Add(mpv-release
         -Dopenal=disabled
         -Dspirv-cross=enabled
         -Dvulkan=enabled
-        -Dlibplacebo=enabled
         -Degl-angle=enabled
     BUILD_COMMAND ${EXEC} LTO_JOB=1 ninja -C <BINARY_DIR>
     INSTALL_COMMAND ""
--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -40,7 +40,6 @@ ExternalProject_Add(mpv
         -Dopenal=disabled
         -Dspirv-cross=enabled
         -Dvulkan=enabled
-        -Dlibplacebo=enabled
         -Degl-angle=enabled
     BUILD_COMMAND ${EXEC} LTO_JOB=1 ninja -C <BINARY_DIR>
     INSTALL_COMMAND ""
